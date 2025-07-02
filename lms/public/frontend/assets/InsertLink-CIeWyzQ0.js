e52928636be507746502e0404c3499386d5d6d3e
Faiullin <osmanfajzulla@gmail.com>
2025-07-02 18:06:22 +0500

    save

---
+++ lms/public/frontend/assets/InsertLink-CIeWyzQ0.js
@@ -0,0 +1,2 @@
+import{au as t,bt as s,bu as a,bv as i}from"./index-DcFDtKzU.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
+//# sourceMappingURL=InsertLink-CIeWyzQ0.js.map
--- lms/public/frontend/assets/InsertLink-DQQR8ivE.js
+++
@@ -1,2 +0,0 @@
-import{au as t,bt as s,bu as a,bv as i}from"./index-DiXMRYjh.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
-//# sourceMappingURL=InsertLink-DQQR8ivE.js.map
